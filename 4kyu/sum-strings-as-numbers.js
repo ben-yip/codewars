6259054c1f95e95fba19ccd38134e0c6c60ec674
BenYip <endless_leaf@163.com>
2018-01-22 00:44:44 +0800

    [unfinished] Sum strings as numbers.

---
+++ 4kyu/sum-strings-as-numbers.js
@@ -0,0 +1,22 @@
+/**
+ * https://www.codewars.com/kata/sum-strings-as-numbers/train/javascript
+ * 
+ * Given the string representations of two integers, return the string representation of the sum of those integers.
+ * 
+ * For example:
+ * sumStrings('1','2') // => '3'
+ * 
+ * A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+ */
+
+/*
+ * 分析：
+ * 这题的坑在于，需要处理超大的整数。
+ */
+
+
+console.log();
+
+/**
+ * 社区解答：
+ */
