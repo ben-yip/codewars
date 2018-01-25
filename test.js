2b14fb6ec9c4a2cfc32cc70585bccf31c285e2d3
BenYip <endless_leaf@163.com>
2018-01-25 11:39:53 +0800

    test

--- test.js
+++ test.js
@@ -1,44 +1,9 @@
 /**
  * TEMP TESTS
  */
+let hour = 2;
 
-/**
- *
- * let n = 5
- * how to output a (n-1)! like this as follows:
- * ------
- * 54321
- * 5321
- * 521
- * 51
- * ------
- * 4321
- * 421
- * 41
- * ------
- * 321
- * 31
- * ------
- * 21
- * ------
- * 1
- */
+// hour = hour ? hour + ' hour' + hour > 1 ? 's' : '' : '';
+hour = hour ? hour + ' hour' + (hour > 1 ? 's' : '') : '';
 
-let fa = (n, origin) => {
-    if (n > 0) {
-        console.log('----');
-        console.log(origin);
-        for (let i = n; i > 0; i--) {
-            console.log(i);
-        }
-        fa(n - 1, origin);
-    }
-};
-
-// fa(4, 5);
-
-let n = 5;
-for (let i = n; i > 0; i--) {
-    console.log('=========');
-    fa(i - 1, i);
-}
+console.log(hour);
